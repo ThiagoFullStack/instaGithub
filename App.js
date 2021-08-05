b5eb0dec77cd5fe1e372dba5a0d9651ffc0fb745
ThiagoFullStack <thiagobarcos@yahoo.com.br>
2021-08-05 18:10:23 -0300

    inital version.

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image , ScrollView, SafeAreaView, TouchableOpacity} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+function InstaGithubCard({githubUser}){
+  const [liked, setLikeState] = useState(false)
+  return(
+    <View>
+      {/* InstaCardHeader */}
+    <View style={{ flexDirection: 'row', padding: 15, alignItems: 'center'}}>
+    
+    <Image 
+    style={{
+    marginRight: 15,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  }} 
+ source={{
+    uri: `https://github.com/${githubUser}.png`
+}}
+ />
+ <Text>{githubUser}</Text>
+ </View>
+
+ {/* InstaCardImage */}
+ <Image style={{
+     width: '100%',
+     height: 350,
+}} 
+ source={{
+   uri: `https://github.com/${githubUser}.png`
+}}
+ />
+
+ {/* Footer */}
+ <View style={{ flexDirection: 'row', padding: 15, alignItems: 'center'}}>
+  
+  <TouchableOpacity onPress={()=> setLikeState(!liked)}>
+    {liked &&  <Ionicons name="heart-sharp" size={24} color="red"/>}
+    {!liked &&  <Ionicons name="heart-sharp" size={24} color="black" />}
+ </TouchableOpacity>
+
+ </View>
+ {/* Footer */}
+ </View>
+ )
 }
 
+
+export default function App() {
+
+return (
+    <SafeAreaView style={styles.container}>
+  <ScrollView >
+    {
+    [
+    'ThiagoFullStack',
+    'tuliofaria',
+    'gabrielfroes'
+    ].map((githubUser)=>
+      <InstaGithubCard key={githubUser} githubUser={githubUser} />
+    )}
+  </ScrollView>
+  </SafeAreaView>
+);
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+flex: 1,
+backgroundColor: '#fff',
+// alignItems: 'center',
+// justifyContent: 'center'
+
   },
-});
+})
+
